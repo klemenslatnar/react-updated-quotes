306c288634973a26dc59703648770116043ecc95
klemenslatnar <klemen.slatnar06@gmail.com>
2022-08-20 14:38:11 +0200

    Changed State From Array To Object

--- src/App.js
+++ src/App.js
@@ -2,29 +2,25 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [quote, setQuote] = useState([]);
+  const [quote, setQuote] = useState({});
 
   const clickHandler = async (e) => {
     e.preventDefault();
 
     const response = await fetch("https://quotes.rest/qod?language=en");
     const data = await response.json();
-    const fetchedQuote = data.contents.quotes;
+    const fetchedQuote = data.contents.quotes[0];
 
-    if (quote.length >= 1) {
-      return;
-    }
     setQuote(fetchedQuote);
   };
-  console.log(quote)
 
   return (
     <div className="layout">
       <div className="container">
         <blockquote className="quote">
-          {quote.map((quote) => quote.quote)}
+          {quote.quote}
         </blockquote>
-        <div className="author">{quote.map((quote) => quote.author)}</div>
+        <div className="author">{quote.author}</div>
         <button className="btn" onClick={clickHandler}>
           Get The Quote Of The Day
         </button>
