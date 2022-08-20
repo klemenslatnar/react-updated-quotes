efd2ed5a9a5a42b44758f02662b232727890aa3c
klemenslatnar <klemen.slatnar06@gmail.com>
2022-08-20 14:27:44 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState([]);
+
+  const clickHandler = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("https://quotes.rest/qod?language=en");
+    const data = await response.json();
+    const fetchedQuote = data.contents.quotes;
+
+    if (quote.length >= 1) {
+      return;
+    }
+    setQuote(fetchedQuote);
+  };
+  console.log(quote)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="container">
+        <blockquote className="quote">
+          {quote.map((quote) => quote.quote)}
+        </blockquote>
+        <div className="author">{quote.map((quote) => quote.author)}</div>
+        <button className="btn" onClick={clickHandler}>
+          Get The Quote Of The Day
+        </button>
+      </div>
     </div>
   );
 }
